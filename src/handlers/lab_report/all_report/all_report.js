4a7bde1c26c2005965c6331ae919cee82902ef02
Gaurav Negi <code.sofueled@sofueled.com>
2024-03-10 19:48:11 +0530

    all reports

---
+++ src/handlers/lab_report/all_report/all_report.js
@@ -0,0 +1,26 @@
+const { validationResult } = require("express-validator");
+const { logger } = require("../../../utils/logger");
+const { success, failure } = require("../../../utils/response");
+const { parse_response } = require("../../../utils/helper");
+const db_service = require("../../../utils/db/service");
+
+const { get_completed_report, get_all_report } = require("./queries");
+module.exports.getAllReport = async (req, res) => {
+  try {
+    console.log(req.url);
+
+    let result;
+    result = parse_response(
+      await db_service.excute_statement(
+        get_all_report()
+      )
+    );
+
+    // return success
+    return success(200, result, res);
+  } catch (e) {
+    console.log(e);
+    logger.error("Error in customer fetch data ::: ", e);
+    return failure(400, "Internal server error.", res);
+  }
+};
--- src/handlers/lab_report/lab_report_page_handler.js
+++ src/handlers/lab_report/lab_report_page_handler.js
@@ -7,6 +7,7 @@ const { getCompletedReport } = require("./completed_report/completed_report");
 const multer = require('multer');
 
 const { uploadReport } = require("./upload_report/upload_report");
+const { getAllReport } = require("./all_report/all_report");
 
 
 const storage = multer.diskStorage({
@@ -25,6 +26,8 @@ const storage = multer.diskStorage({
 router.get("/getPendingReport",middleware, getPendingReport);
 
 router.get("/getCompletedReport",middleware, getCompletedReport);
+
+router.get("/getAllReport",middleware, getAllReport);
 
 
 router.post("/uploadReport", upload.single('file'), uploadReport);
